8f2affe12e04a4f1b0e2473907ba641c89e513b3
Daniela G <daniela.gruberova@merck.com>
2017-08-08 17:21:27 +0200

    actors collection added

--- Moovie_Beta/Moovie_Beta/ActorsOverview.swift
+++ Moovie_Beta/Moovie_Beta/ActorsOverview.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ActorsOverview: UIView {
+class ActorsOverview: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var actorCOllectionView: UICollectionView!
+    var images = ["actor1", "actor2", "actor3", "actor4" ]
+    
+    func viewDidLoad() {
+//        super.viewDidLoad()
+        
+        self.actorCOllectionView.delegate = self
+        self.actorCOllectionView.dataSource = self
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -17,5 +26,15 @@ class ActorsOverview: UIView {
         // Drawing code
     }
     */
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "actor_cell", for: indexPath) as! MyViewCellCollectionViewCell
+        cell.actorCOllectionPicture.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    
+    
 }
---
+++ Moovie_Beta/Moovie_Beta/MyViewCellCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyViewCellCollectionViewCell.swift
+//  Moovie_Beta
+//
+//  Created by Gruberova, Daniela on 08/08/2017.
+//  Copyright © 2017 Gruberova, Daniela. All rights reserved.
+//
+
+import UIKit
+
+class MyViewCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var actorCOllectionPicture: UIImageView!
+}
