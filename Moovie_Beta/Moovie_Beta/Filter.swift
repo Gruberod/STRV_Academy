92ad9f664fafffff84086bb82ec400b956fee43e
Daniela G <daniela.gruberova@merck.com>
2017-08-09 09:24:29 +0200

    filter table added

--- Moovie_Beta/Moovie_Beta/Filter.swift
+++ Moovie_Beta/Moovie_Beta/Filter.swift
@@ -1,15 +1,55 @@
-//
-//  Filter.swift
-//  Moovie_Beta
-//
-//  Created by Gruberova, Daniela on 08/08/2017.
-//  Copyright © 2017 Gruberova, Daniela. All rights reserved.
-//
+////
+////  Filter.swift
+////  Moovie_Beta
+////
+////  Created by Gruberova, Daniela on 08/08/2017.
+////  Copyright © 2017 Gruberova, Daniela. All rights reserved.
+////
 
 import UIKit
 
-class Filter: UIView {
+class Filter: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var filterOptions: UITableView!
+    
+    @IBOutlet weak var filterLabel: UILabel!
+    
+    let options = ["All Generes", "Drama", "Fantasy", "Documentary", "Sci-Fi", "Action", "Horror", "Adventure"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        filterOptions.delegate = self
+        filterOptions.dataSource = self
+     
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "filter_cell", for: indexPath) as! filterCellTableViewCell
+        let text = options[row]
+        cell.filterLabel.text = text
+        
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
---
+++ Moovie_Beta/Moovie_Beta/filterCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  filterCellTableViewCell.swift
+//  Moovie_Beta
+//
+//  Created by Gruberova, Daniela on 08/08/2017.
+//  Copyright © 2017 Gruberova, Daniela. All rights reserved.
+//
+
+import UIKit
+
+class filterCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var filterLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
